587e3efef08ac578d793f5938aadcd3461bb42c3
olek <g00467581@atu.ie>
2026-02-23 16:31:25 +0000

    Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list-component/list-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('data-binding-app');
+
+
+  // assign variables
+  count : number = 0;
+  showMessage : boolean = false;
+  // create functions
+  IncrementCount()
+  {
+    this.count++;
+  }
+  // toggle the message
+  dblClickToggle() {
+  this.showMessage = !this.showMessage;
+}
+
 }
